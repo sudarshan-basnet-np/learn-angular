86c1f41e4d0106ae94e572acfcfe2af47d68f7fb
sudarshan <sudarshan.basnet546@gmail.com>
2025-05-18 07:05:45 +0545

    standalone-properties

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+
+}
